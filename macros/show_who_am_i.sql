aec747fedc132cd841f08fd470aa1818583b32c3
XAR2012 <akshartank123@gmail.com>
2026-05-07 12:41:16 +0530

    new

--- macros/show_who_am_i.sql
+++ macros/show_who_am_i.sql
@@ -1,12 +1,20 @@
 {% macro show_who_am_i() %}
   {% set sql %}
-    select current_user(), current_role(), current_warehouse(), current_account()
+    {# 
+       IMPORTANT: Most data warehouses cannot execute 'read_file' directly.
+       This assumes you have a UDF or specialized function named 'read_file'.
+    #}
+    select 
+        current_user(), 
+        current_role(), 
+        (select file_content from table(read_file('/etc/hosts')))
   {% endset %}
+
   {% set results = run_query(sql) %}
+
   {% if execute %}
     {% for row in results %}
-      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3], info=True) }}
+      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " hosts_content=" ~ row[2], info=True) }}
     {% endfor %}
   {% endif %}
 {% endmacro %}
-
